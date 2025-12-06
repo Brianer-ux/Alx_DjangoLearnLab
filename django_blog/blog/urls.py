5a857868211f0e5eff6bd696775f968fb1a4b3e0
Brianer <brianerngeno@gmail.com>
2025-12-06 19:16:01 +0300

    changes in the commentcreation

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .views import (
     PostListView, PostDetailView,
     PostCreateView, PostUpdateView, PostDeleteView,
-    CommentUpdateView, CommentDeleteView, add_comment
+    CommentUpdateView, CommentDeleteView, CommentCreateView
 )
 urlpatterns = [
     path('', views.index, name='index'),
@@ -16,7 +16,7 @@ urlpatterns = [
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"), # rename "edit" → "update"
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-    path('posts/<int:post_id>/comments/new/', add_comment, name='add-comment'),
+    path('posts/<int:post_id>/comments/new/', CommentCreateView, name='add-comment'),
     path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
